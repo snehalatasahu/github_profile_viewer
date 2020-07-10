2b3ed5ec4c24d71a3f302aec8e36356c3a5b0c94
Snehalata Sahu <sneha16sahu@gmail.com>
2020-07-10 13:30:39 +0530

    Updates

---
+++ precache-manifest.436e42600f978e73ba312ed082daad84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41d2696d80549011c5419e449df0893",
+    "url": "/github_profile_viewer/index.html"
+  },
+  {
+    "revision": "e7d8fac38fea1fafab07",
+    "url": "/github_profile_viewer/static/css/2.79dd3d24.chunk.css"
+  },
+  {
+    "revision": "29e2d1894802792f2f2f",
+    "url": "/github_profile_viewer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7d8fac38fea1fafab07",
+    "url": "/github_profile_viewer/static/js/2.f5b321eb.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/github_profile_viewer/static/js/2.f5b321eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e2d1894802792f2f2f",
+    "url": "/github_profile_viewer/static/js/main.8629d71c.chunk.js"
+  },
+  {
+    "revision": "f2ae16efa194d45bda09",
+    "url": "/github_profile_viewer/static/js/runtime-main.edeaaf0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github_profile_viewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
